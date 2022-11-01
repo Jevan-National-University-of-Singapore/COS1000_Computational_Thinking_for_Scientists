ef63146611bdcec993ac05c38e34341ef5e1a99c
Javern Goh <javerngoh@gmail.com>
2022-11-01 13:30:33 +0800

    initial commit

---
+++ Labs/Lab 4/myMod.py
@@ -0,0 +1,10 @@
+def world(choice):
+    if (choice == 1):
+        print("Hello world 1!")
+    elif(choice == 2):
+        print("Hello world 2!")
+    else: 
+        print("Hello all worlds!")
+        
+def compute(num1, num2, num3):
+    print((num1 + num2)**num3)
